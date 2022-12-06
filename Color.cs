7a4ff0015311ef530d60483dbb07ce85f4c90d4c
robtherobot12 <robert.kvasov@gmail.com>
2022-12-06 11:22:54 +0000

    Added Color functionality

--- Color.cs
+++ Color.cs
@@ -1,5 +1,15 @@
 namespace KVSV.Metaverse {
     public class Color {
-        
+        public float r { get; set; }
+        public float g { get; set; }
+        public float b { get; set; }
+        public float a { get; set; }
+
+        public Color(float r, float g, float b, float a) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = a;
+        }
     }
 }
