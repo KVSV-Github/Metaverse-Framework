a92a84b9ae53b2813c168400114350a7576bd66f
robtherobot12 <robert.kvasov@gmail.com>
2022-12-06 09:29:16 +0000

    refactor: Refactored all code (#4)

--- Component.cs
+++ Component.cs
@@ -2,8 +2,8 @@ namespace KVSV.Metaverse {
     public interface IComponent {}
 
     public record Transform : IComponent {
-        public Vector3 Position;
-        public Vector3 Rotation;
-        public Vector3 Scale;
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+        public Vector3 Scale { get; set; }
     }
 }
