d820a3e90e15e1e6cad19bc3395259099c8daa60
robtherobot12 <robert.kvasov@gmail.com>
2022-12-06 10:26:24 +0000

    feat: New components

--- Component.cs
+++ Component.cs
@@ -6,4 +6,27 @@ namespace KVSV.Metaverse {
         public Vector3 Rotation { get; set; }
         public Vector3 Scale { get; set; }
     }
+
+    public record DefaultPlayer : IComponent {
+        public float Speed { get; set; }
+        public float JumpPower { get; set; }
+    }
+
+    public record Sound : IComponent {
+        public bool Spatial { get; set; }
+        public float Radius { get; set; }
+        public string SoundPath { get; set; }
+        public float Volume { get; set; }
+    }
+
+    public record PointLight : IComponent {
+        public float Radius { get; set; }
+        public float Brightness { get; set; }
+        public Color LightColor { get; set; }
+    }
+
+    public record DirectionalLight : IComponent {
+        public Color LightColor { get; set; }
+        public float Brightness { get; set; }
+    }
 }
